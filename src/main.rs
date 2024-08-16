78dcb7b84ded52d2890b19773064d1d26a90fd87
Zhao Guangjian <zgj0315@gmail.com>
2024-08-16 16:11:10 +0800

    ollama client

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().with_line_number(true).init();
+    let client = reqwest::ClientBuilder::new().build()?;
+    let url = "http://127.0.0.1:11434/api/tags";
+    let rsp = client.get(url).send().await?;
+    let text = rsp.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&text)?;
+    let str_pretty = serde_json::to_string_pretty(&json)?;
+    log::info!("{}", str_pretty);
+
+    let url = "http://127.0.0.1:11434/api/chat";
+    let req_body = serde_json::json!({
+      "model": "gemma2:27b",
+      "messages": [
+        {
+          "role": "user",
+          "content": "why is the sky blue?"
+        }
+      ]
+    });
+    let rsp = client.post(url).body(req_body.to_string()).send().await?;
+    if rsp.status().is_success() {
+        let mut stream = rsp.bytes_stream();
+        while let Some(chunk) = stream.next().await {
+            match chunk {
+                Ok(bytes) => {
+                    let json: serde_json::Value = serde_json::from_slice(&bytes)?;
+                    let content = json["message"]["content"].as_str().unwrap_or_default();
+                    print!("{}", content);
+                    // log::info!("{}", content);
+                }
+                Err(e) => {
+                    eprintln!("Error while reading chunk: {:?}", e);
+                }
+            }
+        }
+    } else {
+        log::error!("Request failed with status: {}", rsp.status());
+    }
+    Ok(())
+}
