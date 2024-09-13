5a76d4792d15aaf52d1656c16031d090b6bd57aa
Zhao Guangjian <zgj0315@gmail.com>
2024-09-13 08:58:32 +0800

    save

---
+++ src/generate.rs
@@ -0,0 +1,18 @@
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().with_line_number(true).init();
+    let client = reqwest::ClientBuilder::new().build()?;
+    let url = "http://127.0.0.1:11434/api/generate";
+    let req_body = serde_json::json!({
+      "model": "llama3.1",
+      "prompt": "Why is the sky blue?",
+      "stream": false
+    });
+    log::info!("\nurl: {}\nreq_body: {}", url, req_body);
+    let rsp = client.post(url).body(req_body.to_string()).send().await?;
+    let text = rsp.text().await?;
+    let json: serde_json::Value = serde_json::from_str(&text)?;
+    let str_pretty = serde_json::to_string_pretty(&json)?;
+    log::info!("{}", str_pretty);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,31 @@ async fn main() -> anyhow::Result<()> {
     // let str_pretty = serde_json::to_string_pretty(&json)?;
     // log::info!("{}", str_pretty);
 
-    // let url = "http://127.0.0.1:11434/api/generate";
-    // let req_body = serde_json::json!({
-    //   "model": "llama3.1",
-    //   "prompt": "Why is the sky blue?"
-    // });
-    // log::info!("\nurl: {}\nreq_body: {}", url, req_body);
-    // let rsp = client.post(url).body(req_body.to_string()).send().await?;
-    // if rsp.status().is_success() {
-    //     let mut stream = rsp.bytes_stream();
-    //     while let Some(chunk) = stream.next().await {
-    //         match chunk {
-    //             Ok(bytes) => {
-    //                 let json: serde_json::Value = serde_json::from_slice(&bytes)?;
-    //                 let content = json["response"].as_str().unwrap_or_default();
-    //                 print!("{}", content);
-    //                 // log::info!("{}", content);
-    //             }
-    //             Err(e) => {
-    //                 eprintln!("Error while reading chunk: {:?}", e);
-    //             }
-    //         }
-    //     }
-    // } else {
-    //     log::error!("Request failed with status: {}", rsp.status());
-    // }
+    let url = "http://127.0.0.1:11434/api/generate";
+    let req_body = serde_json::json!({
+      "model": "llama3.1",
+      "prompt": "Why is the sky blue?"
+    });
+    log::info!("\nurl: {}\nreq_body: {}", url, req_body);
+    let rsp = client.post(url).body(req_body.to_string()).send().await?;
+    if rsp.status().is_success() {
+        let mut stream = rsp.bytes_stream();
+        while let Some(chunk) = stream.next().await {
+            match chunk {
+                Ok(bytes) => {
+                    let json: serde_json::Value = serde_json::from_slice(&bytes)?;
+                    let content = json["response"].as_str().unwrap_or_default();
+                    print!("{}", content);
+                    // log::info!("{}", content);
+                }
+                Err(e) => {
+                    eprintln!("Error while reading chunk: {:?}", e);
+                }
+            }
+        }
+    } else {
+        log::error!("Request failed with status: {}", rsp.status());
+    }
 
     // let req_body = serde_json::json!({
     //   "model": "llama3.1",
